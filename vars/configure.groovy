a0f192e4fa7f6d33810324945e115312cb13bd47
egortrue <estrukhin@gmail.com>
2023-05-05 14:06:02 +0300

    test

--- vars/configure.groovy
+++ vars/configure.groovy
@@ -7,9 +7,9 @@ def call() {
     echo "JOB_URL = ${env.JOB_URL}"
     echo "BUILD_URL = ${env.BUILD_URL}"
 
-    Pipeline.type = ${env.JOB_NAME.contains('/') ? Pipeline.Type.MULTIBRANCH : Pipeline.Type.CLASSIC}
+    Pipeline.type = env.JOB_NAME.contains('/') ? Pipeline.Type.MULTIBRANCH : Pipeline.Type.CLASSIC
 
-    echo "Pipeline Type = $Pipeline.type"
+    echo "Pipeline Type = ${Pipeline.type}"
 
 //    if (multiBranch) {
 //        cleanWs()
