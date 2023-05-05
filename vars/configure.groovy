778dc0e0f27482fe3d0c17402de4c568764b4fef
egortrue <estrukhin@gmail.com>
2023-05-05 13:44:58 +0300

    test

--- vars/configure.groovy
+++ vars/configure.groovy
@@ -3,14 +3,16 @@ def call() {
 
     echo "JOB_NAME = ${env.JOB_NAME}"
     echo "JOB_BASE_NAME = ${env.JOB_BASE_NAME}"
+    echo "JOB_URL = ${env.JOB_URL}"
+    echo "BUILD_URL = ${env.BUILD_URL}"
 
-    if (multiBranch) {
-        cleanWs()
-        checkout scm
-        String filePath = ".ci/"
-        readFile()
-    } else {
-        libraryResource()
-        String filePath = ""
-    }
+//    if (multiBranch) {
+//        cleanWs()
+//        checkout scm
+//        String filePath = ".ci/"
+//        readFile()
+//    } else {
+//        libraryResource()
+//        String filePath = ""
+//    }
 }
