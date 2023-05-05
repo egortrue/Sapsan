bbba25bb7329ab595e387bea72f256f68e496ad1
egortrue <estrukhin@gmail.com>
2023-05-06 00:28:58 +0300

    test

--- src/sapsan/utils/Colors.groovy
+++ src/sapsan/utils/Colors.groovy
@@ -1,9 +1,12 @@
 package sapsan.utils
 
+import com.cloudbees.groovy.cps.NonCPS
+
 class Colors {
 
     static String reset = "\\e[0m"
 
+    @NonCPS
     static String red(String message) {
         return "\\e[31m$message$reset"
     }
