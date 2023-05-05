e1e470b765bab4f62a9aee38f2a8dba93af7618c
egortrue <estrukhin@gmail.com>
2023-05-06 00:34:59 +0300

    test

--- src/sapsan/core/SapsanException.groovy
+++ src/sapsan/core/SapsanException.groovy
@@ -6,8 +6,8 @@ import sapsan.utils.Logging
 class SapsanException extends Exception {
 
     SapsanException(String message) {
-        Logging.error(Colors.red("SAPSAN: $message"))
         super(message)
+        Logging.error(Colors.red("SAPSAN: $message"))
     }
 
 }
