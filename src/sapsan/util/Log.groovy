5919d5105acb5d3ecfe790ad23a4eaf3db96b179
egortrue <estrukhin@gmail.com>
2023-05-07 23:52:15 +0300

    test

--- src/sapsan/util/Log.groovy
+++ src/sapsan/util/Log.groovy
@@ -7,18 +7,18 @@ import sapsan.core.Context
  */
 final class Log extends Context {
 
-  static void info(String text) {
-    env.echo(Color.green(text))
-  }
+    static void info(String text) {
+        pipelineScript.echo(Color.green(text))
+    }
 
-  static void warning(String text) {
-    // TODO: Добавить метку Warning для Stage
-    // TODO: Сохранять все предупреждения в отдельный файл/переменную
-    env.echo(Color.yellow("[Warning] $text"))
-  }
+    static void warning(String text) {
+        // TODO: Добавить метку Warning для Stage
+        // TODO: Сохранять все предупреждения в отдельный файл/переменную
+        pipelineScript.echo(Color.yellow("[Warning] $text"))
+    }
 
-  static void error(String text) {
-    // TODO: Добавить метку Error для Stage и останавливать Pipeline
-    env.echo(Color.red("[Error] $text"))
-  }
+    static void error(String text) {
+        // TODO: Добавить метку Error для Stage и останавливать Pipeline
+        pipelineScript.echo(Color.red("[Error] $text"))
+    }
 }
