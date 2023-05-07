676fcead15293b418bb3cdb163e1cdb46e0d3ade
egortrue <estrukhin@gmail.com>
2023-05-07 23:41:02 +0300

    test

--- vars/execute.groovy
+++ vars/execute.groovy
@@ -2,8 +2,8 @@ import sapsan.core.Context
 import sapsan.jenkins.Pipeline
 import sapsan.util.Log
 
-def call(env) {
-    Context.script = env
+def call() {
+    Context.script = this
     Pipeline.run {
         Log.info("log message")
         Log.warning("warning message")
