1f3118138ac86ef85269f47532a8e5f43b7960e8
egortrue <estrukhin@gmail.com>
2023-05-07 22:01:45 +0300

    test

--- src/sapsan/jenkins/Pipeline.groovy
+++ src/sapsan/jenkins/Pipeline.groovy
@@ -6,45 +6,45 @@ import sapsan.core.Context
 
 class Pipeline extends Context {
 
-  enum Type {
-    NOT_DEFINED,
-    CLASSIC,
-    MULTIBRANCH
-  }
-
-  enum Task {
-    NOT_DEFINED,
-    BUILD,
-    DELIVERY,
-    TESTING,
-    DEPLOYMENT,
-  }
-
-  static List<Stage> stages = new ArrayList<Stage>()
-  static Type type = Type.NOT_DEFINED
-  static Task task = Task.NOT_DEFINED
-
-  /**
-   * Запуск трубы
-   * @param node метка агента Jenkins
-   * @param closure выполняемые действия
-   */
-  static void run(String node = 'linux', Closure closure) {
-    script.node(node) {
-      script.ansiColor('xterm') {
-        closure.call()
-//        stages.each { stage -> stage.call()}
-      }
+    enum Type {
+        NOT_DEFINED,
+        CLASSIC,
+        MULTIBRANCH
     }
-  }
+
+    enum Task {
+        NOT_DEFINED,
+        BUILD,
+        DELIVERY,
+        TESTING,
+        DEPLOYMENT,
+    }
+
+    static List<Stage> stages = new ArrayList<Stage>()
+    static Type type = Type.NOT_DEFINED
+    static Task task = Task.NOT_DEFINED
+
+    /**
+     * Запуск трубы
+     * @param node метка агента Jenkins
+     * @param closure выполняемые действия
+     */
+    static void run(String node = 'linux', Closure closure) {
+        script.node(node) {
+            script.ansiColor('xterm') {
+                closure.call()
+//        stages.each { stage -> stage.call()}
+            }
+        }
+    }
 
 
-  /**
-   * Обертка создания шага пайплайна
-   * @see Stage
-   */
-  static void stage(String name, @ClosureParams(value = SimpleType, options = "Stage") Closure steps) {
-    stages << new Stage(name, steps)
-  }
+    /**
+     * Обертка создания шага пайплайна
+     * @see Stage
+     */
+    static void stage(String name, @ClosureParams(value = SimpleType, options = "Stage") Closure steps) {
+        stages << new Stage(name, steps)
+    }
 
 }
