060c653ad187dc866b387ea71909ec8736a622c6
egortrue <estrukhin@gmail.com>
2023-05-07 17:55:46 +0300

    test

--- vars/run.groovy
+++ vars/run.groovy
@@ -3,7 +3,7 @@ import sapsan.core.Context
 import sapsan.utils.Logging
 
 def call(String type = "build") {
-    Context.instance.script = this
+    Context.script = this
     node("linux") {
         ansiColor('xterm') {
             Context.warning("warning message")
