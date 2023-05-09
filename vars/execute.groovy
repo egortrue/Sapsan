9df114461d336fa5c91a29e88d50d5f39fe8df1c
egortrue <estrukhin@gmail.com>
2023-05-09 03:31:17 +0300

    test

--- vars/execute.groovy
+++ vars/execute.groovy
@@ -3,10 +3,5 @@ import sapsan.jenkins.Pipeline
 import sapsan.util.Log
 
 def call() {
-    Context.script = this
-    Pipeline.run {
-        Log.info("log message")
-        Log.warning("warning message")
-        Log.error("error message")
-    }
+
 }
