6fb914cdae70d94f5a4f3fd41d1949f5a9868fe1
egortrue <estrukhin@gmail.com>
2023-05-09 05:52:30 +0300

    test

--- src/sapsan/Context.groovy
+++ src/sapsan/Context.groovy
@@ -10,7 +10,7 @@ abstract class Context {
      * Корневой восходящий скрипт, содержащийся в /vars. Содержит шаги трубы.
      * Позволяет использовать команды Jenkins, прописанные в /src/pipeline.gdsl
      */
-    static volatile Script script
+    protected static Script script
 
 //  /**
 //   * Инициализация всех свойств объекта.
