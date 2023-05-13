7fc1350882a136bcb209b7c5d88b9155ac85d8bb
egortrue <estrukhin@gmail.com>
2023-05-13 18:28:45 +0300

    test

--- src/sapsan/module/Module.groovy
+++ src/sapsan/module/Module.groovy
@@ -9,6 +9,7 @@ abstract class Module extends Context {
 
     Module() {
         //Map parameters = Pipeline.staticParameters[this.class.simpleName]
+        Map parameters = [:]
         //Log.info "Create component \"${this.class.simpleName}\"\n" +
         //"with parameters: ${JsonOutput.prettyPrint(JsonOutput.toJson(parameters))}"
         initParameters(parameters)
