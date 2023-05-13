885e307cf3a90b48aff3a0456b9d2ef9e6f02d37
egortrue <estrukhin@gmail.com>
2023-05-13 22:10:08 +0300

    test

--- src/sapsan/module/Module.groovy
+++ src/sapsan/module/Module.groovy
@@ -6,11 +6,11 @@ import sapsan.core.Context
 abstract class Module extends Context {
 
     Module() {
-        //Map parameters = Pipeline.staticParameters[this.class.simpleName]
-        Map parameters = [:]
-        //Log.info "Create component \"${this.class.simpleName}\"\n" +
-        //"with parameters: ${JsonOutput.prettyPrint(JsonOutput.toJson(parameters))}"
-        initParameters(parameters)
+//        //Map parameters = Pipeline.staticParameters[this.class.simpleName]
+//        Map parameters = [:]
+//        //Log.info "Create component \"${this.class.simpleName}\"\n" +
+//        //"with parameters: ${JsonOutput.prettyPrint(JsonOutput.toJson(parameters))}"
+//        initParameters(parameters)
     }
 
     @NonCPS
