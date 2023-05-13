cfc1b5d0c1e8094a8a9022e784420a10b1420084
egortrue <estrukhin@gmail.com>
2023-05-13 20:33:33 +0300

    test

--- src/sapsan/module/scm/Git.groovy
+++ src/sapsan/module/scm/Git.groovy
@@ -38,6 +38,7 @@ class Git extends Module {
     static void checkout() {
         Pipeline.stage("Checkout SCM") {
             Log.info(instance.info)
+            Log.info(instance.info)
 
             if (Pipeline.type == Pipeline.Type.MULTIBRANCH) {
                 script.checkout script.scm
