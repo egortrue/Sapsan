7953b0eac4bd2e32dde3b5423a7109300db8b8f3
egortrue <estrukhin@gmail.com>
2023-05-14 03:56:56 +0300

    test

--- src/sapsan/core/Job.groovy
+++ src/sapsan/core/Job.groovy
@@ -4,10 +4,10 @@ import com.cloudbees.groovy.cps.NonCPS
 
 class Job extends Context {
 
-    @Lazy(soft = true) static String name = script.env.JOB_NAME
-    @Lazy(soft = true) static String project = script.env.JOB_NAME
-    @Lazy(soft = true) static String branch = script.env.BRANCH_NAME
-    @Lazy(soft = true) static String url = script.env.BUILD_URL
+    @Lazy static String name = "${script.env.JOB_NAME}"
+    @Lazy static String project = "${script.env.JOB_NAME}"
+    @Lazy static String branch = "${script.env.BRANCH_NAME}"
+    @Lazy static String url = "${script.env.BUILD_URL}"
 
     static String getPath() {
         name.split('/')[0..-1].join('/')
@@ -16,7 +16,7 @@ class Job extends Context {
     static String getBaseUrl() {
         url.split('/')[0..2].join('/')
     }
-    
+
     static String getInfo() {
         """
         [Job Information]
