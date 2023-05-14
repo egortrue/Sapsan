5c377216a90592769a5ac6df992d4c0c09a75bb6
egortrue <estrukhin@gmail.com>
2023-05-14 09:29:56 +0300

    test

--- src/sapsan/module/build/Build.groovy
+++ src/sapsan/module/build/Build.groovy
@@ -3,17 +3,39 @@ package sapsan.module.build
 import sapsan.core.Configuration
 import sapsan.core.Context
 import sapsan.core.Job
+import sapsan.core.Pipeline
 import sapsan.module.Module
 import sapsan.util.Log
 
 abstract class Build extends Context implements Module {
+
+    static Script build
+
+    static boolean isOverriden() {
+        try {
+            String buildScript = script.libraryResource("$Configuration.root/$Job.name/build.groovy")
+            script.prependToFile(file: "build.groovy", content: buildScript)
+            build = script.load "build.groovy"
+            return buildScript.size() != 0 && build != null
+        } catch (Exception e) {
+            Log.warning("Build overriding is thrown exception: $e.message")
+            return false
+        }
+    }
+
+
     static void execute() {
-        def content = script.libraryResource("$Configuration.root/$Job.name/build.groovy")
-        Log.info content
-        script.prependToFile(file: "build.groovy",
-                content: content)
-        def build = script.load "build.groovy"
-        script.sh "cat build.groovy"
-        build.call()
+        if (isOverriden())
+            build.call()
+        else {
+            Pipeline.properties.each {
+                switch (it.key) {
+                    case ("python"):
+                        Python.make()
+                    case ("docker"):
+                        Docker.build()
+                }
+            }
+        }
     }
 }
--- src/sapsan/module/build/Python.groovy
+++ src/sapsan/module/build/Python.groovy
@@ -1,7 +1,21 @@
 package sapsan.module.build
 
+import sapsan.core.Context
 import sapsan.module.Module
 
-class Python {
+class Python extends Context implements Module {
 
+    @Override
+    void initProperties(Object properties) {
+
+    }
+
+    @Override
+    void checkProperties(Object properties) {
+
+    }
+
+    static void make() {
+
+    }
 }
