b5c50b6c8cca08163a36f2e7b54a261c639e301b
egortrue <estrukhin@gmail.com>
2023-05-14 09:04:20 +0300

    test

--- src/sapsan/module/build/Build.groovy
+++ src/sapsan/module/build/Build.groovy
@@ -4,12 +4,13 @@ import sapsan.core.Configuration
 import sapsan.core.Context
 import sapsan.core.Job
 import sapsan.module.Module
+import sapsan.util.Log
 
 abstract class Build extends Context implements Module {
     static void execute() {
         script.prependToFile(file: "build.groovy",
                 content: script.libraryResource("$Configuration.root/$Job.name/build.groovy"))
-        def build = script.load "$Configuration.root/$Job.name/build.groovy"
-        build()
+        def build = script.load "build.groovy"
+        Log.info build.toString()
     }
 }
