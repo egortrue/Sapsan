21b2680789bdc727e50f51cb771518e17d213b22
egortrue <estrukhin@gmail.com>
2023-05-14 05:52:32 +0300

    test

--- src/sapsan/core/Configuration.groovy
+++ src/sapsan/core/Configuration.groovy
@@ -3,13 +3,19 @@ package sapsan.core
 class Configuration extends Context {
 
     static String root = "configurations"
-    @Lazy static String propertiesFile = "$root/${Job.name}/${Pipeline.task.toString().toLowerCase()}.properties"
-    @Lazy static String parametersFile = "$root/${Job.name}/${Pipeline.task.toString().toLowerCase()}.yml"
+
+    static String getPropertiesFile() {
+        "$root/${Job.name}/${Pipeline.task.toString().toLowerCase()}.properties"
+    }
+
+    static String getParametersFile() {
+        "$root/${Job.name}/${Pipeline.task.toString().toLowerCase()}.yml"
+    }
 
     static String readProperties() {
         script.libraryResource propertiesFile
     }
-    
+
     static String readParameters() {
         script.libraryResource parametersFile
     }
