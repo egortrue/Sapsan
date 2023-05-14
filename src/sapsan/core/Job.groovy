516fa4e2d7e2fbaa408921caffbcf510a8c92fe7
egortrue <estrukhin@gmail.com>
2023-05-14 03:22:31 +0300

    test

--- src/sapsan/core/Job.groovy
+++ src/sapsan/core/Job.groovy
@@ -3,19 +3,20 @@ package sapsan.core
 class Job extends Context {
 
     @Lazy static String name = script.env.JOB_NAME
+    @Lazy static String project = script.env.JOB_NAME
     @Lazy static String branch = script.env.BRANCH_NAME
+    @Lazy static String path = name.split('/')[0..-1].join('/')
     @Lazy static String url = script.env.BUILD_URL
     @Lazy static String baseUrl = url.split('/')[0..2].join('/')
-    @Lazy static String path = name.split('/')[0..-1].join('/')
 
     static String getInfo() {
         """
         [Job Information]
         Job.name=$name
         Job.branch=$branch
+        Job.path=$path
         Job.url=$url
         Job.baseUrl=$baseUrl
-        Job.path=$path
         """.stripIndent()
     }
 }
