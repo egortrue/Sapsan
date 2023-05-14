3c3974133a565583b097fa1780ed5a6d58ab539a
egortrue <estrukhin@gmail.com>
2023-05-14 19:27:14 +0300

    test

--- resources/configurations/projectA/service-1/build.groovy
+++ resources/configurations/projectA/service-1/build.groovy
@@ -1,7 +1,15 @@
-import sapsan.util.Log
 import groovy.transform.Field
+import sapsan.util.Log
 
 @Field String name = "Service-1 Build"
+
+void initProperties(Map properties) {
+
+}
+
+void checkProperties(Map properties) {
+
+}
 
 void call() {
     Log.info("This is custom build script. It's invoked by 'CustomBuild' class")
