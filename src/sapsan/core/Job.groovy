f94496dd4da9714b8a7945aec110b8e12e4a8e71
egortrue <estrukhin@gmail.com>
2023-05-14 03:22:42 +0300

    test

--- src/sapsan/core/Job.groovy
+++ src/sapsan/core/Job.groovy
@@ -13,6 +13,7 @@ class Job extends Context {
         """
         [Job Information]
         Job.name=$name
+        Job.project=$project
         Job.branch=$branch
         Job.path=$path
         Job.url=$url
