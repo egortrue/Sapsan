4c97a3b96d417f1502fa27ad8d38e1dbb6a74636
egortrue <estrukhin@gmail.com>
2023-05-14 08:17:54 +0300

    test

--- src/sapsan/module/Module.groovy
+++ src/sapsan/module/Module.groovy
@@ -3,9 +3,9 @@ package sapsan.module
 import com.cloudbees.groovy.cps.NonCPS
 import sapsan.core.Context
 
-abstract class Module extends Context {
+interface Module {
 
     @NonCPS
-    protected abstract void initProperties(Map properties)
+    void initProperties(Map properties)
 
 }
--- src/sapsan/module/build/Docker.groovy
+++ src/sapsan/module/build/Docker.groovy
@@ -1,19 +1,23 @@
 package sapsan.module.build
 
 import sapsan.core.Pipeline
+import sapsan.core.Context
 import sapsan.module.Module
 import sapsan.util.Log
 
-class Docker extends Module {
+class Docker extends Context implements Module {
 
-    private String image
-    private String file
+    static private String name
+    static private String image
+    static private String file
 
     @Override
-    protected void initProperties(Map properties) {
-        image = properties["docker.image"]
-        file = properties["docker.file"]
+    void initProperties(Map properties) {
+        name = properties.key
+        image = properties.value["image"]
+        file = properties.value["dockerfile"]
 
+        assert name != null
         assert image != null
         assert file != null
     }
@@ -21,7 +25,9 @@ class Docker extends Module {
     static void build() {
         Pipeline.stage("Build Docker") {
             Pipeline.properties["docker"].each {
-                Log.info it.value.toString()
+
+                initProperties(it)
+
             }
         }
     }
