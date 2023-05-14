402723602658ac3fc24575436272566079835944
egortrue <estrukhin@gmail.com>
2023-05-14 04:01:31 +0300

    test

--- src/sapsan/core/Job.groovy
+++ src/sapsan/core/Job.groovy
@@ -9,15 +9,15 @@ class Job extends Context {
     @Lazy static String branch = "${script.env.BRANCH_NAME}"
     @Lazy static String url = "${script.env.BUILD_URL}"
 
-    @NonCPS
-    static String getPath() {
-        name.split('/')[0..-1].join('/')
-    }
-
-    @NonCPS
-    static String getBaseUrl() {
-        url.split('/')[0..2].join('/')
-    }
+//    @NonCPS
+//    static String getPath() {
+//        name.split('/')[0..-1].join('/')
+//    }
+//
+//    @NonCPS
+//    static String getBaseUrl() {
+//        url.split('/')[0..2].join('/')
+//    }
 
     @NonCPS
     static String getInfo() {
@@ -26,9 +26,7 @@ class Job extends Context {
         Job.name=$name
         Job.name=$project
         Job.branch=$branch
-        Job.path=$path
         Job.url=$url
-        Job.baseUrl=$baseUrl
         """.stripIndent()
     }
 }
