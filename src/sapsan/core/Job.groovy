760e46d9cb171e11e0bdc23a3f0d0a42b41e8272
egortrue <estrukhin@gmail.com>
2023-05-15 03:56:00 +0300

    test

--- src/sapsan/core/Job.groovy
+++ src/sapsan/core/Job.groovy
@@ -1,28 +1,35 @@
 package sapsan.core
 
+import com.cloudbees.groovy.cps.NonCPS
+
 class Job extends Context {
 
     /**
      *  Уникальный путь до джобы
      */
+    @NonCPS
     static String getName() {
         if (script.env.BRANCH_NAME)
             return script.env.JOB_NAME.split('/')[0..-2].join('/')
         return script.env.JOB_NAME
     }
 
+    @NonCPS
     static String getBranch() {
         script.env.BRANCH_NAME
     }
 
+    @NonCPS
     static String getUrl() {
         script.env.BUILD_URL
     }
 
+    @NonCPS
     static String getBaseUrl() {
         url.split('/')[0..2].join('/')
     }
 
+    @NonCPS
     static String getInfo() {
         """
         [Job Information]
