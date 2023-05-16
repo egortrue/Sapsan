0eacc2c07780baa3d6df8ae1e9e78861e236469d
egortrue <estrukhin@gmail.com>
2023-05-17 02:36:24 +0300

    test

--- src/sapsan/core/Config.groovy
+++ src/sapsan/core/Config.groovy
@@ -10,30 +10,42 @@ final class Config extends Context {
     private static Map parameters
     private static Map properties
 
+    static String getGlobalParametersFile() {
+        "$root/global/$parametersFilename"
+    }
+
+    static String getParametersFile() {
+        "$root/custom/${Job.name}/$parametersFilename"
+    }
+
+    static String getPropertiesFile() {
+        "$root/custom/${Job.name}/$propertiesFilename"
+    }
+
     static Map getGlobalParameters() {
         if (globalParameters == null)
-            globalParameters = parse("$root/global/$parametersFilename")
+            globalParameters = parse(globalParametersFile)
         return globalParameters
     }
 
     static Map getParameters() {
         if (parameters == null)
-            parameters = parse("$root/custom/${Job.name}/$parametersFilename")
+            parameters = parse(parametersFile)
         return parameters
     }
 
     static Map getProperties() {
         if (properties == null)
-            properties = parse("$root/custom/${Job.name}/$propertiesFilename")
+            properties = parse(propertiesFile)
         return properties
     }
 
     static String getInfo() {
         """
         [Config Information]
-        globalParametersPath="$root/global/$parametersFilename"
-        parametersPath="$root/custom/${Job.name}/$parametersFilename"
-        propertiesPath="$root/custom/${Job.name}/$propertiesFilename"
+        globalParametersFile="$globalParametersFile"
+        parametersFile="$parametersFile"
+        propertiesFile="$propertiesFile"
         """.stripIndent()
     }
 
