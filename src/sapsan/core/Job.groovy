5829948a59646258ef3349bddb1e1b2d633797b9
egortrue <estrukhin@gmail.com>
2023-05-17 01:21:53 +0300

    test

--- src/sapsan/core/Job.groovy
+++ src/sapsan/core/Job.groovy
@@ -1,13 +1,15 @@
 package sapsan.core
 
 import com.cloudbees.groovy.cps.NonCPS
+import groovy.transform.Memoized
 
 class Job extends Context {
 
     /**
-     *  Уникальный путь до джобы
+     *  Полный путь до джобы
      */
     @NonCPS
+    @Memoized
     static String getName() {
         if (Context.pipeline.env.BRANCH_NAME)
             return Context.pipeline.env.JOB_NAME.split('/')[0..-2].join('/')
@@ -15,21 +17,25 @@ class Job extends Context {
     }
 
     @NonCPS
+    @Memoized
     static String getBranch() {
         Context.pipeline.env.BRANCH_NAME
     }
 
     @NonCPS
+    @Memoized
     static String getUrl() {
         Context.pipeline.env.BUILD_URL
     }
 
     @NonCPS
+    @Memoized
     static String getBaseUrl() {
         url.split('/')[0..2].join('/')
     }
 
     @NonCPS
+    @Memoized
     static String getInfo() {
         """
         [Job Information]
