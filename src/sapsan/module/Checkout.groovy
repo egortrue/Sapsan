48827ed91d627fd5a60313671486344707a6a360
egortrue <estrukhin@gmail.com>
2023-05-17 04:26:30 +0300

    test

--- src/sapsan/module/Checkout.groovy
+++ src/sapsan/module/Checkout.groovy
@@ -9,14 +9,14 @@ class Checkout extends Module {
     String url
     String branch
 
-//    @Override
-//    protected Map getProperties() {
-//        if (Pipeline.type == Pipeline.Type.CLASSIC) {
-//            return super.properties
-//        } else if (Pipeline.type == Pipeline.Type.MULTIBRANCH) {
-//            return null
-//        }
-//    }
+    @Override
+    protected Map getProperties() {
+        if (Pipeline.type == Pipeline.Type.CLASSIC) {
+            return super.properties
+        } else if (Pipeline.type == Pipeline.Type.MULTIBRANCH) {
+            return null
+        }
+    }
 
     @Override
     protected void checkProperties(Map properties) {
@@ -46,5 +46,6 @@ class Checkout extends Module {
                                    branches         : [[name: branch]],
                                    userRemoteConfigs: [[credentialsId: 'my-cred', url: url]]])
         Context.pipeline.sh "ls -al"
+        Context.pipeline.sh "exit 1"
     }
 }
