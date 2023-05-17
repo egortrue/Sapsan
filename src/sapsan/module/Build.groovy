bc6931da7bfab85a4d6ad3acb23a0c28e5939edf
egortrue <estrukhin@gmail.com>
2023-05-17 05:21:08 +0300

    test

--- src/sapsan/module/Build.groovy
+++ src/sapsan/module/Build.groovy
@@ -11,7 +11,7 @@ final class Build extends Module {
 
     @Override
     protected String getStageName() {
-        return script.stageName ?: super.stageName
+        return script.getStageName() ?: super.stageName
     }
 
     @Override
