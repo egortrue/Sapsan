33adeaf3af90608afa898a2daea863249795cc6a
egortrue <estrukhin@gmail.com>
2023-05-17 05:46:36 +0300

    test

--- resources/configurations/custom/projectA/service-1/build.groovy
+++ resources/configurations/custom/projectA/service-1/build.groovy
@@ -1,16 +1,24 @@
+import groovy.transform.Field
 import sapsan.core.Config
+import sapsan.core.Context
 import sapsan.util.Log
 
-void checkProperties(Map properties) {
+@Field String src
+@Field String dest
 
+void checkProperties(Map properties) {
+    assert properties["src"] != null
+    assert properties["dest"] != null
 }
 
 void initProperties(Map properties) {
-    Log.var properties
+    src = properties["src"]
+    dest = properties["dest"]
 }
 
 void execute() {
     Log.info("This is custom build script. This script located in '${Config.projectDir}'")
+    Context.pipeline.zip(dir: src, zipFile: dest)
 }
 
 return this
